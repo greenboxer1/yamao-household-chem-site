4be0d56a117186117183004101c2b8a24b52858a
greenboxer1 <greenboxer@list.ru>
2025-05-27 14:27:44 +0300

    dsdsdsds

--- restart.sh
+++ restart.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
+echo '------------------------Copy repo------------------------'
+git pull origin main
+
+echo '------------------------Switch to Node.js 20------------------------'
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 nvm use 20
 
 echo '------------------------Stop web server------------------------'
 pm2 stop server
-
-echo '------------------------Copy repo------------------------'
-git pull origin main
 
 echo '------------------------Build frontend------------------------'
 cd ~/yamao-household-chem-site/household-chem-site/frontend
