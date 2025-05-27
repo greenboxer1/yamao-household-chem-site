8c13d52b04b0579d2fb0fcea4b028c0114afa7df
greenboxer1 <greenboxer@list.ru>
2025-05-27 11:34:46 +0300

    dfd

--- restart.sh
+++ restart.sh
@@ -1,3 +1,12 @@
 git pull origin main
+
+export NVM_DIR="$HOME/.nvm"
+export NODE_VERSION="v20.18.0"
+export PATH="$NVM_DIR/versions/node/$NODE_VERSION/bin:$PATH"
+echo "Using Node: $(which node)"
+echo "Node version: $(node -v)"
+echo "Using PM2: $(which pm2)"
+
 cd household-chem-site/backend
-~/.nvm/versions/node/v20.18.0/bin/pm2 restart server.js
+
+pm2 restart server.js
