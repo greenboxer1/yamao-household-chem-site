4b1733c712ee23d045ab9aed3a5fb33018ce34e5
greenboxer1 <greenboxer@list.ru>
2025-05-27 14:09:38 +0300

    sdfsdfsdfsdf

--- restart.sh
+++ restart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
+pm2 stop server
 git pull origin main
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 nvm use 20
 cd ~/yamao-household-chem-site/household-chem-site/frontend
-npm run build
-cd household-chem-site/backend
-pm2 restart server.js
+vite build
+pm2 start server
