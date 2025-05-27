1b151707c3c8ddc3bdcc0657edff9d64734d579d
greenboxer1 <greenboxer@list.ru>
2025-05-27 14:33:06 +0300

    fix deploy x9328942

--- restart.sh
+++ restart.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-set -e
-echo '------------------------Copy repo------------------------'
-git pull origin main
-
-echo '------------------------Switch to Node.js 20------------------------'
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-nvm use 20
-
-echo '------------------------Stop web server------------------------'
-pm2 stop server
-
-echo '------------------------Build frontend------------------------'
-cd ~/yamao-household-chem-site/household-chem-site/frontend
-vite build
-
-echo '------------------------Start web server------------------------'
-cd ~/yamao-household-chem-site/household-chem-site/backend
-pm2 start server
+pm2 restart server
