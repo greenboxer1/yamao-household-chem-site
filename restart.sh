c6bd838cfb242b77c1607cb970dfe3910febc091
greenboxer1 <greenboxer@list.ru>
2025-05-27 11:38:56 +0300

    final restart script

--- restart.sh
+++ restart.sh
@@ -1,13 +1,6 @@
 git pull origin main
-
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
-
 nvm use 20
-
-echo "Using Node: $(which node)"
-echo "Node version: $(node -v)"
-echo "Using PM2: $(which pm2)"
-
 cd household-chem-site/backend
 pm2 restart server.js
